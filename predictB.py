52872ffa2f262ac8bf7a10b0ba7ec256fc1a7882
dapaohou <dapaohou@gmail.com>
2018-01-20 16:08:53 +0800

    testB

---
+++ predictB.py
@@ -0,0 +1,19 @@
+from cleanData import *
+
+df = pd.read_csv(".\\data\\testB.csv")
+out = df[['ID']]
+df = drop_fill(df)
+df = read_Tools_Dictionary(df)
+delcols = np.loadtxt(".\\data\\delcols.txt", dtype=bytes).astype(str)
+df.drop(delcols, 1, inplace=True)
+
+X = np.array(df.values)
+print(X.shape)
+X = scale_load(X)
+
+model = joblib.load(".\\model\\GBR.m")
+y = model.predict(X)
+# print(y[0:20])
+out['y'] = y
+print('out', out)
+out.to_csv('.\\result\\116B_GBR.csv')
